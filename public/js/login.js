cdc6cba5e14466f207c5cdb668607bf5f008f6c3
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-21 21:17:28 -0600

    api added

--- public/js/login.js
+++ public/js/login.js
@@ -2,17 +2,20 @@ document.querySelector('#login-form').addEventListener('submit', event => {
     event.preventDefault();
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
+    console.log('Request Payload:', JSON.stringify({ email, password }));
+
     fetch('/api/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     })
     .then(response => {
-      if (response.redirected) {
+      console.log('Response:', response);
+      if (response && response.redirected) {
         window.location.href = response.url;
       }
     })
     .catch(error => {
       console.error('Error:', error);
     });
-  });
+})
